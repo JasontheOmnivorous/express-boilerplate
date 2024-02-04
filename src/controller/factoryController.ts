341b2b5ff1ac03753a82cbaff20e1ac0b81d717f
JasontheOmnivorous <marsjason485@gmail.com>
2024-02-04 11:59:58 +0630

    findAll factory function

--- src/controller/factoryController.ts
+++ src/controller/factoryController.ts
@@ -2,7 +2,27 @@ import { NextFunction, Request, Response } from "express";
 import { Model } from "mongoose";
 import { catchAsync } from "../middleware/catchAsync";
 import { UserType } from "../types/user";
+import ApiFeatures from "../utils/apiFeatures";
 import { filterBody } from "../utils/helpers";
+
+export const findAll = (Model: Model<UserType>) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const features = new ApiFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .paginate()
+      .limitFields();
+
+    const documents = await features.query;
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: documents,
+      },
+    });
+  });
+};
 
 // type of Model may differ from what model we're using
 export const crateOne = (Model: Model<UserType>) => {

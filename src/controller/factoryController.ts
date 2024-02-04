128ae6c2b36d58a922d86ec9dc671d5b8fdc53a3
JasontheOmnivorous <marsjason485@gmail.com>
2024-02-04 12:09:46 +0630

    deleteOne factory

--- src/controller/factoryController.ts
+++ src/controller/factoryController.ts
@@ -82,3 +82,17 @@ export const updateOne = (Model: Model<UserType>) => {
     });
   });
 };
+
+export const deleteOne = (Model: Model<UserType>) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const document = await Model.findByIdAndDelete(req.params.id);
+
+    if (!document)
+      return next(new AppError(`No ${Model} found with that id.`, 400));
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  });
+};
